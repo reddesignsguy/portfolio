cad0d2f1f642611ea8b8de6d9aafebcedcfcac8f
Albany Patriawan <49921782+reddesignsguy@users.noreply.github.com>
2025-03-04 23:02:30 -0800

    fixed interia page

--- app/inertia/page.tsx
+++ app/inertia/page.tsx
@@ -4,21 +4,19 @@ import ProjectPageTemplate from "../components/ProjectPageTemplate";
 export default function Inertia() {
   return (
     <ProjectPageTemplate
-      title={"The Abyss"}
-      description={"Descend into the abyss"}
-      descriptionHiglighted={"without colliding into obstacles."}
+      title={"Inertia"}
+      description={"Play as an astronaut and solve 2D puzzles by"}
+      descriptionHiglighted={"controlling the gravity of objects."}
       highlights={[
-        "Developed with C++ and OpenFrameworks",
-        "Physics system based on forces allows satisfying movement of spaceship in all 6 directions, including yaw rotation",
-        "Cave map is spatially partitioned with an Octree for efficient collision detection",
-        "Collision resolution (bouncing) is based on the normals of colliding faces and the velocity of the player",
-        "Particle color and size are dynamically calculated and rendered using GLSL Shaders",
-        "All models were created in Maya",
+        "Developed for a 7-day game jam using C# and Unity",
+        "Utilized an event-driven system and object-oriented programming (OOP) to decouple logic between input, player-character, the gravity selection process, and interactive objects, increasing maintainability",
+        "Polished game feel with stunning user-interaction FX like gravity selection, object outlining, and proximity-based object highlighting using shader graphs",
+        "Integrated matrix transforms, animations, sounds, and particle systems of multiple sprites using structural design patterns",
       ]}
-      youtube={"https://www.youtube.com/embed/NHVrczwoOJw"}
-      itch={"https://vexeo.itch.io/the-abyss"}
+      youtube={"https://www.youtube.com/embed/sCcTyYLRaNw"}
+      itch={"https://vexeo.itch.io/who-is-out-there"}
       itchWidget={
-        "https://itch.io/embed/3011404?border_width=0&bg_color=111111&fg_color=ffffff&link_color=2ce8f4&border_color=111111"
+        "https://itch.io/embed/2898848?border_width=0&bg_color=111111&fg_color=ffffff&link_color=2ce8f4&border_color=111111"
       }
     />
   );
